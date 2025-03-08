503e7a45fc5efd1d1822437c9babeb95cb17e1bb
Enri <avcenrile@users.noreply.github.com>
2025-03-09 04:11:39 +0800

    Arrivo First Version (Not Working Yet)

---
+++ app/src/main/java/arrivo/demo/arrivo/LandingPage.java
@@ -0,0 +1,38 @@
+package arrivo.demo.arrivo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity
+public class LandingPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.landing_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Click
+    public void logInLandingBtn(){
+        LogIn_.intent(this).start();
+    }
+
+    @Click
+    public void signUpLandingBtn(){
+        SignUp_.intent(this).start();
+    }
+}
+
---
+++ app/src/main/java/arrivo/demo/arrivo/SignUp.java
@@ -0,0 +1,21 @@
+package arrivo.demo.arrivo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity
+public class SignUp extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.sign_up);
+    }
+}
